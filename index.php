1617212b65404f9fd256a5717c36536377404fc5
Marco Riva <marco.riva@techrain.eu>
2018-11-22 17:48:36 +0100

    add password intent

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ $query_result = $dialogflow_post_data['queryResult'];
 $intent_name = $query_result['intent']['displayName'];
 $parameters = $query_result['parameters'];
 $user_text = $query_result['queryText'];
+$output_context = $query_result['outputContexts'];
+$old_parameters = $output_context['parameters'];
 
 // log input
 $log_file = "/var/www/html/bot/bot.log";
@@ -16,7 +18,7 @@ $content = date('Y-m-d H:i:s') . " DialogFlow Post:\n" . $dialogflow_post . "\n"
 file_put_contents($log_file, $content, FILE_APPEND);
 
 // get output
-$answer = getAnswer($intent_name, $parameters, $user_text);
+$answer = getAnswer($intent_name, $parameters, $user_text, $old_parameters);
 $output_json = json_encode(array(
 		"fulfillmentText" => $answer,
 		"fulfillmentMessages" => array(array("text" => array("text" => array($answer)))),
@@ -31,21 +33,30 @@ file_put_contents($log_file, $content, FILE_APPEND);
 header('Content-type: application/json; charset=utf-8');
 echo $output_json;
 
-function getAnswer($intent_name, $parameters, $user_text) {
+function getAnswer($intent_name, $parameters, $user_text, $old_parameters) {
 	$answer = "Puoi ripetere la richiesta?";
 
-	switch ($intent_name){
-        case ('prenotazione - username'):
-            {
-                if ($parameters['any'] == ''){
-                    $answer = "allora scegli un username per registrarti";
-                }
-                else {
-                    $answer = "ciao " . $user_text . ". bentornato";
-                }
+	switch ($intent_name) {
+        case ('prenotazione - username'): {
+            if ($parameters['any'] == '') {
+                $answer = "allora scegli un username per registrarti";
+            } else {
+                $username = $parameters['any'];
+                $answer = "ciao " . $user_text . ", dimmi la tua password.";
             }
-            break;
-        default : $answer = "yes";
+        }
+        break;
+        case ('prenotazione - username - password') : {
+            $username = $old_parameters['any.original'];
+            $password = $parameters['anypas'];
+            $url = 'http://itesla.quinary.it/phpScheduleIt/Web/Services/Authentication/Authenticate';
+            http_post($url . '?username=' . $username . '&password=' . $password);
+        }
+
+
+
+
+
     }
     /*
 	if (strcmp('SimNumberIntent', $intent_name) == 0) {
@@ -77,16 +88,38 @@ function getAnswer($intent_name, $parameters, $user_text) {
 	return $answer;
 }
 
+function http_post($url){
+    $postdata = array();
 
-function getSimNumber($company) {
-	if (strcmp('Techcompany', $company) == 0) return 200;
-	if (strcmp('Datacompany', $company) == 0) return 100;
-	return -1;
+    if(strpos($url,"?") !== FALSE){
+        $qsr = array();
+        $url_r = explode("?", $url);
+        $url = $url_r[0];
+        parse_str($url_r[1], $qsr);
+        $postdata = http_build_query($qsr);
+    }
+
+    $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+
+    $context  = stream_context_create($opts);
+    $output = @file_get_contents($url, false, $context);
+
+    if($output === FALSE) {
+        if (function_exists('curl_init')){
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $output = curl_exec($ch);
+            curl_close ($ch);
+        }
+    }
+    return $output;
 }
-
-function getSimExpence($company) {
-        if (strcmp('Techcompany', $company) == 0) return 4000;
-        if (strcmp('Datacompany', $company) == 0) return 2500;
-        return -1;
-}
-
