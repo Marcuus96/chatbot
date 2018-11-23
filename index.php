5b940f69c789bf7da37a47716a8a4f8eefbcc78e
Marco Riva <marco.riva@techrain.eu>
2018-11-23 10:23:21 +0100

    post curl

--- index.php
+++ index.php
@@ -20,9 +20,9 @@ file_put_contents($log_file, $content, FILE_APPEND);
 // get output
 $answer = getAnswer($intent_name, $parameters, $user_text, $old_parameters);
 $output_json = json_encode(array(
-		"fulfillmentText" => $answer,
-		"fulfillmentMessages" => array(array("text" => array("text" => array($answer)))),
-		"source" => "wesenseit.quinary.it"
+    "fulfillmentText" => $answer,
+    "fulfillmentMessages" => array(array("text" => array("text" => array($answer)))),
+    "source" => "wesenseit.quinary.it"
 ), JSON_PRETTY_PRINT);
 
 // log output
@@ -33,28 +33,54 @@ file_put_contents($log_file, $content, FILE_APPEND);
 header('Content-type: application/json; charset=utf-8');
 echo $output_json;
 
-function getAnswer($intent_name, $parameters, $user_text, $old_parameters) {
-	$answer = "Puoi ripetere la richiesta?";
+function getAnswer($intent_name, $parameters, $user_text, $old_parameters)
+{
+    $answer = "Puoi ripetere la richiesta?";
 
-	switch ($intent_name) {
-        case ('prenotazione - username'): {
-            if ($parameters['any'] == '') {
-                $answer = "allora scegli un username per registrarti";
-            } else {
-                $username = $parameters['any'];
-                $answer = "ciao " . $user_text . ", dimmi la tua password.";
+    switch ($intent_name) {
+        case ('prenotazione - username'):
+            {
+                if ($parameters['any'] == '') {
+                    $answer = "allora scegli un username per registrarti";
+                } else {
+                    $username = $parameters['any'];
+                    $answer = "ciao " . $user_text . ", dimmi la tua password.";
+                }
             }
-        }
-        break;
-        case ('prenotazione - username - password') : {
-            $username = $old_parameters['any.original'];
-            $password = $parameters['anypas'];
-            $url = 'http://itesla.quinary.it/phpScheduleIt/Web/Services/Authentication/Authenticate';
-            http_post($url . '?username=' . $username . '&password=' . $password);
-        }
+            break;
+        case ('prenotazione - username - password') :
+            {
+                $username = $old_parameters['any.original'];
+                $password = $parameters['anypas'];
 
+                // The data to send to the API
+                $postData = array(
+                    'username' => $username,
+                    'password' => $password
+                );
 
+                // Setup cURL
+                $ch = curl_init('http://itesla.quinary.it/phpScheduleIt/Web/Services/Authentication/Authenticate');
+                curl_setopt_array($ch, array(
+                    CURLOPT_POST => TRUE,
+                    CURLOPT_RETURNTRANSFER => TRUE,
+                    CURLOPT_POSTFIELDS => json_encode($postData)
+                ));
 
+                // Send the request
+                $response = curl_exec($ch);
+
+                // Check for errors
+                if ($response === FALSE) {
+                    $answer = curl_error($ch);
+                    break;
+                }
+
+                // Decode the response
+                $responseData = json_decode($response, TRUE);
+                $answer = "login effettuato: " . $responseData['isAuthenticated'];
+            }
+            break;
 
 
     }
@@ -85,41 +111,5 @@ function getAnswer($intent_name, $parameters, $user_text, $old_parameters) {
         }
     */
 
-	return $answer;
-}
-
-function http_post($url){
-    $postdata = array();
-
-    if(strpos($url,"?") !== FALSE){
-        $qsr = array();
-        $url_r = explode("?", $url);
-        $url = $url_r[0];
-        parse_str($url_r[1], $qsr);
-        $postdata = http_build_query($qsr);
-    }
-
-    $opts = array('http' =>
-        array(
-            'method'  => 'POST',
-            'header'  => 'Content-type: application/x-www-form-urlencoded',
-            'content' => $postdata
-        )
-    );
-
-    $context  = stream_context_create($opts);
-    $output = @file_get_contents($url, false, $context);
-
-    if($output === FALSE) {
-        if (function_exists('curl_init')){
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $output = curl_exec($ch);
-            curl_close ($ch);
-        }
-    }
-    return $output;
+    return $answer;
 }
