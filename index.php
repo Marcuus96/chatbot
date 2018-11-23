248c73c820a16401697957b0d31f2b2f60448ba1
Marco Riva <marco.riva@techrain.eu>
2018-11-23 17:14:20 +0100

    bh

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ function getAnswer($intent_name, $parameters, $user_text, $old_parameters)
                     if($responseData === NULL)
                         $answer = "ERROR" . json_last_error();
                     else
-                    $answer = "login effettuato: " . $responseData . "bye.";
+                    $answer = "login effettuato: " . $responseData->userId . "bye.";
                 }
             }
             break;
