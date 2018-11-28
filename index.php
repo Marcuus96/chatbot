54815aae20ae1bf5bb280dd77061022bc2058f0a
Marco Riva <marco.riva@techrain.eu>
2018-11-28 10:37:35 +0100

    change header

--- index.php
+++ index.php
@@ -122,9 +122,9 @@ function getAnswer($intent_name, $parameters, $user_text, $old_parameters)
                 );
 
                 $header = array(
-                    "Content-Type" => "application/json",
-                    "X-phpScheduleIt-SessionToken" => $session,
-                    "X-phpScheduleIt-UserId" => $user
+                    "Content-Type: application/json",
+                    "X-phpScheduleIt-SessionToken: " . $session,
+                    "X-phpScheduleIt-UserId: " . $user
                 );
 
 
@@ -133,7 +133,7 @@ function getAnswer($intent_name, $parameters, $user_text, $old_parameters)
                 curl_setopt_array($ch, array(
                     CURLOPT_POST => TRUE,
                     CURLOPT_RETURNTRANSFER => TRUE,
-                    CURLOPT_HTTPHEADER => json_encode($header),
+                    CURLOPT_HTTPHEADER => $header,
                     CURLOPT_POSTFIELDS => json_encode($postData),
                     CURLOPT_FAILONERROR => TRUE
                 ));
@@ -154,7 +154,7 @@ function getAnswer($intent_name, $parameters, $user_text, $old_parameters)
 
                     if ($responseData === NULL)
                         //$answer = "ERROR" . json_last_error() . ' ' . $responseData['referenceNumber'];
-                        $answer = json_encode($header);
+                        $answer = $header;
                     else {
                         if ($responseData['isPendingApproval'] === TRUE)
                             $answer = 'prenotazione avvenuta con successo grazie';
